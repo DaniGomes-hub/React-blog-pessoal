2b607e27098bb62a26e52b057b7f8551fef060dc
DaniGomes-hub <165850415+DaniGomes-hub@users.noreply.github.com>
2024-06-26 16:35:24 -0300

    Home do blog

---
+++ src/pagina/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://fazendoanossafesta.com.br/wp-content/uploads/2022/09/Flork-Professor-zueira-png.png" 
+            alt="Imagem Tela Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
