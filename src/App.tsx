1188b9b9524d3d2cac717a08759a1c021613e948
DaniGomes-hub <165850415+DaniGomes-hub@users.noreply.github.com>
2024-06-27 11:54:51 -0300

    Flexbox e Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-import React, { useState } from 'react';
-import Home from './pagina/home/Home';
+import React from 'react';
 import './App.css';
+import Home from './pagina/home/Home';
 
 function App() {
-  const [valor, setValor] = useState(0);
 
-  function handleClick() {
-    setValor(valor + 1);
-  }
   return (
-    //  <Home  title="Componente Home"
-    //         description="Este é um componente Home que recebe props."/>
-    <div>
-      <p>O valor é: {valor}</p>
-      <button onClick={handleClick}>Adicionar 1</button>
-    </div>
-  );
+    <>
+    <Home/>
+    </>
+);
 }
 
 export default App;
